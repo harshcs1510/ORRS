b76e73b1c4c2a7df19f4ddc432396340f7d66401
harshcs1510 <117885181+harshcs1510@users.noreply.github.com>
2023-04-05 04:20:21 +0530

    Add files via upload

---
+++ assets/inc/footer.php
@@ -0,0 +1,4 @@
+<div class="splash-footer">
+    &copy; 2022 - <?php echo date ('Y');?>
+    Train Ticket Reservation System | Developed By Harsh Shetty(4MT20CS069) & Krishii Karkera(4MT20CS087)
+</div>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<!--
+	Train Ticket Reservation System
+-->
+<html>
+	<head>
+		<title>Train Ticket Reservation System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">Train Ticket Reservation System</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">User Login</a></li>
+								<li><a href="employee/emp-login.php">Employee Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span><img src="https://c8.alamy.com/zooms/9/850369be1a284a4ea94c6810f6e3141c/bhtncr.jpg" width="100" height="100"></span></div>
+							<h2>Train Ticket Reservation System</h2>
+							<p>Train Tickets at your fingertips!</a></p>
+						</div>
+					</section>
+
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Get in touch</h2>
+							
+							<ul class="contact">
+									
+								</li>
+								<li class="icon solid fa-phone">(+91) 9140435170</li>
+								<li class="icon solid fa-envelope"><a href="#">harshcs1510@gmail.com</a></li>
+							</ul>
+							<ul class="copyright">
+								<li>&copy; Train Ticket Reservation System.</li><li>Developed By: Harsh Shetty(4MT20CS069) & Krishii Karkera(4MT20CS087)</li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
